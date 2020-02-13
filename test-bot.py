0c7a6fab0b6650766d057f588ca40d1d30bfaec4
frekfrekfrek <louloutebreizh29@gmail.com>
2020-02-13 22:14:57 +0100

    add minimal code

---
+++ test-bot.py
@@ -0,0 +1,21 @@
+# permissions integer : 330816
+
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$about'):
+        await message.channel.send('Hello ! I am a work-in-progress bot for vote automation. ')
+        await message.channel.send('One of my planned features is to automatize server-related decisions decided by these votes. ')
+        await message.channel.send('Right now I cannot do any of this yet, but be on the lookout for updates !')
+
+client.run('token')
